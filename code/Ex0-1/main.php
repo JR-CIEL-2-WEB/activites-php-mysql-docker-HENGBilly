e05d61b0fb634764d45f6a9a3e805c5f9a396f98
HENGBilly <billy.heng@lycee-jeanrostand.fr>
2024-11-04 11:57:38 -0500

    Exercice 0.2

--- code/Ex0-1/main.php
+++ code/Ex0-1/main.php
@@ -6,4 +6,17 @@ $tab = [15, 18, 12, 20, 14];
 $moyenneNotes = moyenne($tab);
 
 echo "La moyenne des notes est : " . $moyenneNotes;
+require_once 'statistique.php';
+
+$salaires = [1500, 4500, 2200, 1500, 3300, 1800, 1700, 2000, 4000];
+sort($salaires);
+
+echo "Moyenne des salaires : " . moyenne($salaires) . "\n";
+echo "Médiane des salaires : " . mediane($salaires) . "\n";
+$salaireNicolas = 2200;
+if ($salaireNicolas < mediane($salaires)) {
+    echo "Nicolas n'est pas dans les moins bien payés de l'entreprise.\n";
+} else {
+    echo "Nicolas est dans les moins bien payés de l'entreprise.\n";
+}
 ?>
--- code/Ex0-1/statistique.php
+++ code/Ex0-1/statistique.php
@@ -9,4 +9,17 @@ function moyenne($notes) {
     $nombreDeNotes = count($notes); 
     return $somme / $nombreDeNotes;
 }
+function mediane($tab){
+    $n = count($tab);
+    sort($tab);
+    
+    if ($n % 2 == 0) {
+        $milieu1 = floor($n / 2) - 1;
+        $milieu2 = floor($n / 2);
+        return ($tab[$milieu1] + $tab[$milieu2]) / 2;
+    } else {
+        $milieu = floor($n / 2);
+        return $tab[$milieu];
+    }
+}
 ?>
