81d921eb46126b23841f0f65dea1785e545e48a6
HENG_Billy <144659596+HENGBilly@users.noreply.github.com>
2024-11-18 16:39:58 +0000

    function tri_selection_ref operatif

--- code/tri/tri_selection.php
+++ code/tri/tri_selection.php
@@ -16,7 +16,23 @@ function tri_selection($t){
     }
     return $t; 
 }
+function tri_selection_ref(&$t){
+    $n = count($t);
+    for($i = 0; $i < $n - 1; $i++){ 
+        $min = $i;
+        for($j = $i + 1; $j < $n; $j++){
+            if($t[$j] < $t[$min]){
+                $min = $j;
+            }
+        }
+        if ($min != $i) {
+            $temp = $t[$i];
+            $t[$i] = $t[$min];
+            $t[$min] = $temp;
+        }
+    }
 
+}
 $t = [5, 3, 8, 2, 9, 1, 7, 4, 6];
 $test = tri_selection($t);
 
