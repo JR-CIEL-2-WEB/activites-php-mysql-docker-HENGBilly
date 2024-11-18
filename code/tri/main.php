4d7d947379958bfccf8772a803632936ffccf906
HENG_Billy <144659596+HENGBilly@users.noreply.github.com>
2024-11-18 16:34:43 +0000

    fonction tri_selection opératif

---
+++ code/tri/main.php
@@ -0,0 +1,3 @@
+<?php 
+include 'tri_selection.php';
+?>
---
+++ code/tri/tri_selection.php
@@ -0,0 +1,24 @@
+<?php
+function tri_selection($t){
+    $n = count($t);
+    for($i = 0; $i < $n - 1; $i++){ 
+        $min = $i;
+        for($j = $i + 1; $j < $n; $j++){
+            if($t[$j] < $t[$min]){
+                $min = $j;
+            }
+        }
+        if ($min != $i) {
+            $temp = $t[$i];
+            $t[$i] = $t[$min];
+            $t[$min] = $temp;
+        }
+    }
+    return $t; 
+}
+
+$t = [5, 3, 8, 2, 9, 1, 7, 4, 6];
+$test = tri_selection($t);
+
+echo "Tableau trié par ordre croissant : " . implode(", ", $test);
+?>
