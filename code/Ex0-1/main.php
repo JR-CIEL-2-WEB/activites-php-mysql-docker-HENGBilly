6b606bf8a467a6c284a324beecfd2ee208a9773f
HENG_Billy <144659596+HENGBilly@users.noreply.github.com>
2024-11-18 15:55:50 +0000

    corriger de l'éexo 0.2

--- code/Ex0-1/main.php
+++ code/Ex0-1/main.php
@@ -5,18 +5,18 @@ $tab = [15, 18, 12, 20, 14];
 
 $moyenneNotes = moyenne($tab);
 
-echo "La moyenne des notes est : " . $moyenneNotes;
+echo "La moyenne des notes est : " . $moyenneNotes."</br>";
 require_once 'statistique.php';
 
 $salaires = [1500, 4500, 2200, 1500, 3300, 1800, 1700, 2000, 4000];
 sort($salaires);
 
-echo "Moyenne des salaires : " . moyenne($salaires) . "\n";
-echo "Médiane des salaires : " . mediane($salaires) . "\n";
-$salaireNicolas = 2200;
-if ($salaireNicolas < mediane($salaires)) {
-    echo "Nicolas n'est pas dans les moins bien payés de l'entreprise.\n";
+echo "Moyenne des salaires : " . moyenne($salaires) . "</br>";
+echo "Médiane des salaires : " . mediane($salaires) . "</br>";
+$salaireNicolas = 1100;
+if ($salaireNicolas > mediane($salaires)) {
+    echo "Nicolas n'est pas dans les moins bien payés de l'entreprise.</br>";
 } else {
-    echo "Nicolas est dans les moins bien payés de l'entreprise.\n";
+    echo "Nicolas est dans les moins bien payés de l'entreprise.</br>";
 }
 ?>
