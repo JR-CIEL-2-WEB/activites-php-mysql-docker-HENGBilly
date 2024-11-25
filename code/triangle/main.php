6add9bba02b208969c2a428c066dffc33085c44a
HENG_Billy <144659596+HENGBilly@users.noreply.github.com>
2024-11-25 16:18:25 +0000

    triangle (exercice 1)

---
+++ code/triangle/main.php
@@ -0,0 +1,16 @@
+<?php
+function triangle($n){
+    for ($i = 1; $i <= $n; $i++) {
+        for ($j = 1; $j <= $i; $j++) {
+            echo "*";
+        }
+        echo "</br>";
+    }
+}
+
+if (isset($_GET['n'])) {
+    $n = intval($_GET['n']);
+    triangle($n);
+}
+triangle(5);
+?>
